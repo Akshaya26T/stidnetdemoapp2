bcbb6909e2a62a281300198450981d6dbb55f80e
vairakkumaar svs <noreply@singamtech.com>
2026-03-11 14:21:42 -0700

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace stidnetdemoapp2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
